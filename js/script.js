ca40079f280d22843ffe69dbc80c49be7e1ba7cc
Ahmed Amin <109681770+Madsvs123@users.noreply.github.com>
2023-04-24 14:15:55 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let menuBtn = document.getElementById('menuBtn');
+let navMenu = document.getElementById('navMenu');
+
+menuBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('displayOn');
+})
